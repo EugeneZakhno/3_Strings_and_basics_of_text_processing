87a130c5baa6f7b758ac03df70b9de8512070317
EugeneZakhno <zohan1234567@gmail.com>
2019-08-30 22:32:14 +0300

    first commit for Task_02

--- Task02/src/main/java/WordOnLetterChanger.java
+++ Task02/src/main/java/WordOnLetterChanger.java
@@ -1,14 +1,12 @@
+/*    Eng:
+    Change in the string all words "word" to "letter"
+
+    Rus:
+    Заменить в строке все вхождения word на letter
+*/
 public class WordOnLetterChanger {
-
-// Заменить в строке все вхождения word на letter
-
     public static void main(String[] args) {
-
+        String string = "word word hello world force it word. ";
+        System.out.println(string.replace("word", "letter"));
     }
-
-
-
-
-
-
 }
