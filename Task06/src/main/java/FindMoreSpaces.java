ca9109259fc7c6b70050d5542e2d92f7bd0e9ba8
EugeneZakhno <zohan1234567@gmail.com>
2019-09-02 21:25:44 +0300

    First commit for Task_05

---
+++ Task06/src/main/java/FindMoreSpaces.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* Eng:
+   We have a text. Find the largest number of running spaces in it.
+
+   Rus:
+   Дан текст. Найти наибольшее количество идущих продряд пробелов в нём.
+*/
+
+public class FindMoreSpaces {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String strings = reader.readLine();
+            System.out.println(replace(strings));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static int replace(String strings) {
+        int counter = 0;
+        int maxCount = 0;
+
+        for (int i = 0; i < strings.length(); i++) {
+            if (strings.charAt(i) == ' ') {
+                counter++;
+            } else {
+                if (maxCount < counter) {
+                    maxCount = counter;
+                }
+                counter = 0;
+            }
+        }
+        return maxCount;
+    }
+
+}
