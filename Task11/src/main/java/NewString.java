03097195b97f40375f27bfd46c3b7cee5e1289cb
EugeneZakhno <zohan1234567@gmail.com>
2019-09-03 22:11:18 +0300

    First commit for Task_10

--- Task11/src/main/java/NewString.java
+++ Task11/src/main/java/NewString.java
@@ -1,8 +1,28 @@
 
-// Из заданной строки получить новую, повторив каждый символ дважды
+
+/*  Eng:
+    Get a new one from the given string by repeating each character twice
+
+    Rus:
+    Из заданной строки получить новую, повторив каждый символ дважды
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class NewString {
-    public static void main(String[] args) {
 
-    }
+        public static void main(String[] args) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+                String string = reader.readLine();
+               // System.out.println(counter(string, 'a'));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
 }
