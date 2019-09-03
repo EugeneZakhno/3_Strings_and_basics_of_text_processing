21d9d186ed7908cd31df2fc7e4142c0945e3d400
EugeneZakhno <zohan1234567@gmail.com>
2019-09-03 22:02:45 +0300

    First commit for Task_08

---
+++ Task10/src/main/java/MeetA.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*  Eng:
+    Count how many times the letter “a” is found among the characters of the given string.
+
+    Rus:
+    Подсчитать, сколько раз среди символов заданной строки встречается буква “а”.
+*/
+
+public class MeetA {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String string = reader.readLine();
+            System.out.println(counter(string, 'a'));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int counter(String line, char character) {
+        int counter = 0;
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == character) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ Task11/src/main/java/NewString.java
@@ -0,0 +1,8 @@
+
+// Из заданной строки получить новую, повторив каждый символ дважды
+
+public class NewString {
+    public static void main(String[] args) {
+
+    }
+}
