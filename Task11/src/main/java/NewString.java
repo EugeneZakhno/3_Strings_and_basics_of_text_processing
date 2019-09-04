6e134f14e97861535c168b187a27fa39a3354dc7
EugeneZakhno <zohan1234567@gmail.com>
2019-09-04 18:07:59 +0300

    second commit for Task_11

--- Task11/src/main/java/NewString.java
+++ Task11/src/main/java/NewString.java
@@ -1,28 +1,30 @@
-
-
-/*  Eng:
-    Get a new one from the given string by repeating each character twice
-
-    Rus:
-    Из заданной строки получить новую, повторив каждый символ дважды
-*/
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+/*
+    Eng:
+   From the given string, get a new one by repeating each character twice.
+    Rus:
+   Из заданной строки получить новую, повторив каждый символ дважды.
+*/
 public class NewString {
-
-        public static void main(String[] args) {
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-                String string = reader.readLine();
-               // System.out.println(counter(string, 'a'));
-            } catch (IOException e) {
-                e.printStackTrace();
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String strings = reader.readLine();
+            System.out.println(aditing(strings));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static String aditing(String text) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (text != null) {
+            char posSymbol;
+            for (int i = 0; i < text.length(); i++) {
+                posSymbol = text.charAt(i);
+                stringBuilder.append(posSymbol).append(posSymbol);
             }
         }
-
-
-
-
+        return stringBuilder.toString();
+    }
 }
